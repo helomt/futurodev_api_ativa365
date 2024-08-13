8767ace05fc6415ade5201c7f257246c013f937d
Heloisa Tavares <heloisamt@gmail.com>
2024-08-13 18:24:16 -0300

    Habilitando o OpenFeign

--- src/main/java/com/futurodev/ativa365/Ativa365Application.java
+++ src/main/java/com/futurodev/ativa365/Ativa365Application.java
@@ -2,8 +2,10 @@ package com.futurodev.ativa365;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Ativa365Application {
 
 	public static void main(String[] args) {
