b27ffaf021c6c470971dffe161cdb98a20d6eb9c
Heloisa Tavares <heloisamt@gmail.com>
2024-08-13 15:27:39 -0300

    :tada: Fist Commit

---
+++ src/main/java/com/futurodev/ativa365/Ativa365Application.java
@@ -0,0 +1,13 @@
+package com.futurodev.ativa365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ativa365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ativa365Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/futurodev/ativa365/Ativa365ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurodev.ativa365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ativa365ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
