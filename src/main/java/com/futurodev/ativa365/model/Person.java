9b319c731a51abfdaae34b0e90f8f3914619c4d1
Heloisa Tavares <heloisamt@gmail.com>
2024-08-13 19:29:05 -0300

    Alteração no tipo de dado na data, criação dos getters e contrutores

--- src/main/java/com/futurodev/ativa365/model/Person.java
+++ src/main/java/com/futurodev/ativa365/model/Person.java
@@ -1,9 +1,10 @@
 package com.futurodev.ativa365.model;
 
 import com.futurodev.ativa365.model.enums.GenderEnum;
+import com.futurodev.ativa365.model.transport.CreatePersonForm;
 import jakarta.persistence.*;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +26,7 @@ public class Person {
     private String cpf;
 
     @Column(nullable = false)
-    private LocalDateTime brithday;
+    private LocalDate brithday;
 
     @Column(nullable = false, unique = true)
     private String email;
@@ -51,9 +52,69 @@ public class Person {
     @OneToMany(mappedBy = "owner")
     private List<Local> localList = new ArrayList<>();
 
+    public Person(){
+    }
+
+    public Person(CreatePersonForm form){
+        this.name = form.name();
+        this.gender = form.gender();
+        this.cpf = form.cpf();
+        this.brithday = form.birthday();
+        this.email = form.email();
+        this.password = form.password();
+        this.cep = form.cep();
+    }
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public GenderEnum getGender() {
+        return gender;
+    }
 
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getBrithday() {
+        return brithday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public List<Local> getLocalList() {
+        return localList;
+    }
 }
