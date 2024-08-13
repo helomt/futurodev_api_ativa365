90272b08abfe179ab99d91f1acfeae8464d4f9e3
Heloisa Tavares <heloisamt@gmail.com>
2024-08-13 17:43:37 -0300

    Inclusão das anotações da entidade, criação da lista de locais

--- src/main/java/com/futurodev/ativa365/model/Person.java
+++ src/main/java/com/futurodev/ativa365/model/Person.java
@@ -1,23 +1,55 @@
 package com.futurodev.ativa365.model;
 
-import jakarta.persistence.Entity;
+import com.futurodev.ativa365.model.enums.GenderEnum;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
-    private String gender;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(50)")
+    @Enumerated(EnumType.STRING)
+    private GenderEnum gender;
+
+    @Column(nullable = false, unique = true)
     private String cpf;
+
+    @Column(nullable = false)
     private LocalDateTime brithday;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String cep;
+
+    @Column
     private String number;
+
+    @Column
     private String street;
+
+    @Column
     private String complement;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean deleted;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Local> localList = new ArrayList<>();
 
 
 
