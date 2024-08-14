3d8487ea475e63fadbb5b34dfe6dee6192927f12
Heloisa Tavares <heloisamt@gmail.com>
2024-08-14 18:11:12 -0300

    Validação de email e senha para novos cadastros e criação da lista paginada

--- src/main/java/com/futurodev/ativa365/service/PersonService.java
+++ src/main/java/com/futurodev/ativa365/service/PersonService.java
@@ -1,9 +1,14 @@
 package com.futurodev.ativa365.service;
 
+import com.futurodev.ativa365.exceptions.PersonCpfAlreadyExistsException;
+import com.futurodev.ativa365.exceptions.PersonEmailAlreadyExistsException;
 import com.futurodev.ativa365.model.Person;
 import com.futurodev.ativa365.model.transport.CreatePersonForm;
 import com.futurodev.ativa365.model.transport.PersonDTO;
 import com.futurodev.ativa365.repository.PersonRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,9 +19,20 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
-    public PersonDTO createPerson(CreatePersonForm form){
+    @Transactional
+    public PersonDTO createPerson(CreatePersonForm form) throws PersonEmailAlreadyExistsException, PersonCpfAlreadyExistsException {
+        if(this.personRepository.existsByEmail(form.email())){
+            throw new PersonEmailAlreadyExistsException(form.email());
+        }
+        if(this.personRepository.existsByCpf(form.cpf())){
+            throw new PersonCpfAlreadyExistsException(form.cpf());
+        }
         Person persistedPerson = this.personRepository.save(new Person(form));
         return new PersonDTO(persistedPerson);
+    }
+
+    public Page<PersonDTO> listPaginatedPerson(Pageable pageable){
+        return this.personRepository.findAll(pageable).map(PersonDTO::new);
     }
 
 
