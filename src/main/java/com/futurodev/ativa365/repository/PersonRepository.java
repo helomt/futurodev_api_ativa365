90d52c3f8cb10f0816a52058f2077af889bb47bb
Heloisa Tavares <heloisamt@gmail.com>
2024-08-14 18:10:15 -0300

    :sparkles: Criação de duas buscas personalizadas para email e cpf

--- src/main/java/com/futurodev/ativa365/repository/PersonRepository.java
+++ src/main/java/com/futurodev/ativa365/repository/PersonRepository.java
@@ -4,4 +4,8 @@ import com.futurodev.ativa365.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByCpf(String cpf);
 }
