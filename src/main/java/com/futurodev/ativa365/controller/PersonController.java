16c3317b3ec9c68a2378ec6b35d094dda8cb02b5
Heloisa Tavares <heloisamt@gmail.com>
2024-08-14 18:09:09 -0300

    Adição de exceções no POST e Criação de um GET all para consultas eventuais

--- src/main/java/com/futurodev/ativa365/controller/PersonController.java
+++ src/main/java/com/futurodev/ativa365/controller/PersonController.java
@@ -1,14 +1,16 @@
 package com.futurodev.ativa365.controller;
 
+import com.futurodev.ativa365.exceptions.PersonCpfAlreadyExistsException;
+import com.futurodev.ativa365.exceptions.PersonEmailAlreadyExistsException;
 import com.futurodev.ativa365.model.transport.CreatePersonForm;
 import com.futurodev.ativa365.model.transport.PersonDTO;
 import com.futurodev.ativa365.service.PersonService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -23,9 +25,17 @@ public class PersonController {
     }
 
     @PostMapping
-    public ResponseEntity<PersonDTO> createPerson(@RequestBody @Valid CreatePersonForm form, UriComponentsBuilder uriComponentsBuilder){
+    public ResponseEntity<PersonDTO> createPerson(@RequestBody @Valid CreatePersonForm form,
+                                                  UriComponentsBuilder uriComponentsBuilder)
+            throws PersonEmailAlreadyExistsException, PersonCpfAlreadyExistsException {
         PersonDTO response = this.personService.createPerson(form);
         URI uri = uriComponentsBuilder.path("/usuario/{id}").buildAndExpand(response.id()).toUri();
         return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PersonDTO>> listPaginatedPersons(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable){
+        Page<PersonDTO> response = this.personService.listPaginatedPerson(pageable);
+        return response.hasContent() ? ResponseEntity.ok(response): ResponseEntity.noContent().build();
     }
 }
