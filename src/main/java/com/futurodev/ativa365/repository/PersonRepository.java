5690383dff15aa4117fe1d95723864537a33aa58
Heloisa Tavares <heloisamt@gmail.com>
2024-08-14 18:42:01 -0300

    :sparkles: Criação da busca de usuários não deletados

--- src/main/java/com/futurodev/ativa365/repository/PersonRepository.java
+++ src/main/java/com/futurodev/ativa365/repository/PersonRepository.java
@@ -1,6 +1,8 @@
 package com.futurodev.ativa365.repository;
 
 import com.futurodev.ativa365.model.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
@@ -8,4 +10,6 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByCpf(String cpf);
+
+    Page<Person> findDistinctByDeletedFalse(Pageable pageable);
 }
