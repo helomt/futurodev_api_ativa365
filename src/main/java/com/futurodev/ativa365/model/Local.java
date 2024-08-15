64b015aa1a9baa2fc69debc31174aaca818b3a1b
Heloisa Tavares <heloisamt@gmail.com>
2024-08-15 17:26:15 -0300

    Criação dos construtores e dos GETTERs

--- src/main/java/com/futurodev/ativa365/model/Local.java
+++ src/main/java/com/futurodev/ativa365/model/Local.java
@@ -1,6 +1,7 @@
 package com.futurodev.ativa365.model;
 
 import com.futurodev.ativa365.model.enums.ActivityEnum;
+import com.futurodev.ativa365.model.transport.CreateLocalForm;
 import jakarta.persistence.*;
 
 @Entity
@@ -45,5 +46,72 @@ public class Local {
     @Column(columnDefinition = "boolean default false")
     private boolean deleted;
 
+    public Local(){
 
+    }
+
+    public Local(CreateLocalForm form, Person owner){
+        this.name = form.name();
+        this.description = form.description();
+        this.cep = form.cep();
+        this.number = form.number();
+        this.localidade = form.localidade();
+        this.logradouro = form.logradouro();
+        this.uf = form.uf();
+        this.complement = form.complement();
+        this.activity = form.activity();
+        this.owner = owner;
+    }
+
+    public void markAsDeleted(){
+        this.deleted = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public ActivityEnum getActivity() {
+        return activity;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
 }
