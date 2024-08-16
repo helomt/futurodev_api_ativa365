2db9a612f3433384c4ad9e06897e8adb5d373713
Heloisa Tavares <heloisamt@gmail.com>
2024-08-16 18:48:31 -0300

    Sobrecarga do costrutor incluindo versão com email

--- src/main/java/com/futurodev/ativa365/exceptions/PersonNotFoundException.java
+++ src/main/java/com/futurodev/ativa365/exceptions/PersonNotFoundException.java
@@ -4,4 +4,8 @@ public class PersonNotFoundException extends Exception {
     public PersonNotFoundException(Long id) {
         super(String.format("The user for id %d was not found", id));
     }
+
+    public PersonNotFoundException(String email){
+        super("The user for this email was not found: "+email);
+    }
 }
