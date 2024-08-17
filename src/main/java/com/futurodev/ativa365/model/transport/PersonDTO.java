3669dc08ecd379cdd0d10505042a57d0363179d4
Heloisa Tavares <heloisamt@gmail.com>
2024-08-17 11:37:07 -0300

    Inclusão do padrão para a data, alteração de idioma em relação aos dados do endereço

--- src/main/java/com/futurodev/ativa365/model/transport/PersonDTO.java
+++ src/main/java/com/futurodev/ativa365/model/transport/PersonDTO.java
@@ -1,5 +1,6 @@
 package com.futurodev.ativa365.model.transport;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.futurodev.ativa365.model.Person;
 import com.futurodev.ativa365.model.enums.GenderEnum;
 
@@ -9,12 +10,15 @@ public record PersonDTO(Long id,
                         String name,
                         GenderEnum gender,
                         String cpf,
+                        @JsonFormat(pattern = "dd/MM/yyyy")
                         LocalDate birthday,
                         String email,
                         String cep,
                         String number,
                         String street,
-                        String complement) {
+                        String complement,
+                        String city,
+                        String state) {
 
     public PersonDTO(Person person){
         this(
@@ -27,7 +31,9 @@ public record PersonDTO(Long id,
                 person.getCep(),
                 person.getNumber(),
                 person.getStreet(),
-                person.getComplement()
+                person.getComplement(),
+                person.getCity(),
+                person.getState()
         );
     }
 }
