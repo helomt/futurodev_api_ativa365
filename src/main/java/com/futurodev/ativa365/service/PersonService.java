38596c60421511ce013aaee0712353c00678c3e9
Heloisa Tavares <heloisamt@gmail.com>
2024-08-17 11:20:20 -0300

    :sparkles: Criação da busca do endereço pelo cep e validação dos dados retornados

--- src/main/java/com/futurodev/ativa365/service/PersonService.java
+++ src/main/java/com/futurodev/ativa365/service/PersonService.java
@@ -1,11 +1,14 @@
 package com.futurodev.ativa365.service;
 
+import com.futurodev.ativa365.client.ViaCepClient;
+import com.futurodev.ativa365.exceptions.CepNotFoundException;
 import com.futurodev.ativa365.exceptions.PersonCpfAlreadyExistsException;
 import com.futurodev.ativa365.exceptions.PersonEmailAlreadyExistsException;
 import com.futurodev.ativa365.exceptions.PersonNotFoundException;
 import com.futurodev.ativa365.model.Person;
 import com.futurodev.ativa365.model.transport.CreatePersonForm;
 import com.futurodev.ativa365.model.transport.PersonDTO;
+import com.futurodev.ativa365.model.transport.ViaCepDTO;
 import com.futurodev.ativa365.repository.PersonRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
@@ -16,27 +19,36 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class PersonService  implements UserDetailsService {
     private final PersonRepository personRepository;
     private  final PasswordEncoder passwordEncoder;
+    private final ViaCepClient viaCepClient;
 
-    public PersonService(PersonRepository personRepository, PasswordEncoder passwordEncoder) {
+    public PersonService(PersonRepository personRepository, PasswordEncoder passwordEncoder, ViaCepClient viaCepClient) {
         this.personRepository = personRepository;
         this.passwordEncoder = passwordEncoder;
+        this.viaCepClient = viaCepClient;
     }
 
     @Transactional
-    public PersonDTO createPerson(CreatePersonForm form) throws PersonEmailAlreadyExistsException, PersonCpfAlreadyExistsException {
+    public PersonDTO createPerson(CreatePersonForm form) throws PersonEmailAlreadyExistsException, PersonCpfAlreadyExistsException, CepNotFoundException {
         if(this.personRepository.existsByEmail(form.email())){
             throw new PersonEmailAlreadyExistsException(form.email());
         }
         if(this.personRepository.existsByCpf(form.cpf())){
             throw new PersonCpfAlreadyExistsException(form.cpf());
         }
-        //String encodedPassword = this.passwordEncoder.encode(form.password());
-        Person persistedPerson = this.personRepository.save(new Person(form, this.passwordEncoder));
-        return new PersonDTO(persistedPerson);
+        ViaCepDTO address = this.viaCepClient.search(form.cep()).getBody();
+        if(address.cep() == null){
+            throw new CepNotFoundException(form.cep());
+        } else {
+            //String encodedPassword = this.passwordEncoder.encode(form.password());
+            Person persistedPerson = this.personRepository.save(new Person(form, this.passwordEncoder, Objects.requireNonNull(address)));
+            return new PersonDTO(persistedPerson);
+        }
     }
 
     //Puramente para consultas
