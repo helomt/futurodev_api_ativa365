a87ba0abb70fde9d083e1f3116857bb433dae64e
Heloisa Tavares <heloisamt@gmail.com>
2024-08-17 11:31:06 -0300

    Remoçao de alguns campos relacionados ao endereço e adição da anotação valid para cep e cpf

--- src/main/java/com/futurodev/ativa365/model/transport/CreatePersonForm.java
+++ src/main/java/com/futurodev/ativa365/model/transport/CreatePersonForm.java
@@ -1,6 +1,7 @@
 package com.futurodev.ativa365.model.transport;
 
 import com.futurodev.ativa365.model.enums.GenderEnum;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -8,11 +9,11 @@ import java.time.LocalDate;
 
 public record CreatePersonForm(@NotBlank String name,
                                @NotNull GenderEnum gender,
-                               @NotBlank String cpf,
+                               @NotBlank @Valid String cpf,
                                @NotNull LocalDate birthday,
                                @NotBlank String email,
                                @NotBlank String password,
-                               @NotBlank String cep,
+                               @NotBlank @Valid String cep,
                                String number,
                                String complement) {
 }
