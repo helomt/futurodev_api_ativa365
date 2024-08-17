a60f588661583b3477dc9a9f1553b2821c0d5c3e
Heloisa Tavares <heloisamt@gmail.com>
2024-08-16 23:01:45 -0300

    :sparkles: Criação de métodos HTTP GET, GET{id}, UPDATE, DELETE todos com validação de usuário e método GET {all} para buscar todos locais, sem qualquer validação

--- src/main/java/com/futurodev/ativa365/controller/LocalController.java
+++ src/main/java/com/futurodev/ativa365/controller/LocalController.java
@@ -1,17 +1,19 @@
 package com.futurodev.ativa365.controller;
 
+import com.futurodev.ativa365.exceptions.LocalNotFoundException;
 import com.futurodev.ativa365.exceptions.PersonNotFoundException;
 import com.futurodev.ativa365.model.transport.CreateLocalForm;
 import com.futurodev.ativa365.model.transport.LocalDTO;
+import com.futurodev.ativa365.model.transport.UpdateLocalForm;
 import com.futurodev.ativa365.service.LocalService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -37,4 +39,42 @@ public class LocalController {
 
         return ResponseEntity.created(uri).body(response);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<LocalDTO>> listPaginatedLocals(@PageableDefault(size = 10, sort = {"name"})Pageable pageable,
+                                                              @AuthenticationPrincipal UserDetails userInSession) throws PersonNotFoundException {
+       Page<LocalDTO> response = this.localService.listPaginatedLocals(pageable, userInSession);
+       return response.hasContent() ? ResponseEntity.ok(response): ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LocalDTO> getSingleLocal(@PathVariable("id") Long id,
+                                                   @AuthenticationPrincipal UserDetails userInSession) throws LocalNotFoundException {
+        LocalDTO response = this.localService.getSingleLocal(id, userInSession);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<LocalDTO> deleteLocal(@PathVariable("id") Long id,
+                                                @AuthenticationPrincipal UserDetails userInSession) throws LocalNotFoundException {
+        this.localService.deleteLocal(id, userInSession);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LocalDTO> updateLocal(@PathVariable("id") Long id,
+                                                @RequestBody @Valid UpdateLocalForm form,
+                                                @AuthenticationPrincipal UserDetails userInSession) throws LocalNotFoundException{
+        LocalDTO response = this.localService.updateLocal(id, form, userInSession);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<LocalDTO>> listPaginatedLocalsNoOwner(@PageableDefault(size = 10, sort = {"name"})Pageable pageable) {
+        Page<LocalDTO> response = this.localService.listPaginatedLocalsNoOwner(pageable);
+        return response.hasContent() ? ResponseEntity.ok(response): ResponseEntity.noContent().build();
+    }
+
+
 }
--- src/main/java/com/futurodev/ativa365/service/LocalService.java
+++ src/main/java/com/futurodev/ativa365/service/LocalService.java
@@ -1,15 +1,21 @@
 package com.futurodev.ativa365.service;
 
+import com.futurodev.ativa365.exceptions.LocalNotFoundException;
 import com.futurodev.ativa365.exceptions.PersonNotFoundException;
 import com.futurodev.ativa365.model.Local;
 import com.futurodev.ativa365.model.Person;
 import com.futurodev.ativa365.model.transport.CreateLocalForm;
 import com.futurodev.ativa365.model.transport.LocalDTO;
+import com.futurodev.ativa365.model.transport.UpdateLocalForm;
 import com.futurodev.ativa365.repository.LocalRepository;
 import com.futurodev.ativa365.repository.PersonRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class LocalService {
@@ -23,11 +29,46 @@ public class LocalService {
         this.personRepository = personRepository;
     }
 
+    public LocalDTO getSingleLocal(Long id, UserDetails userInSession) throws LocalNotFoundException {
+        return this.localRepository.findByIdAndDeletedFalseAndOwnerEmail(id, userInSession.getUsername())
+                .map(LocalDTO::new)
+                .orElseThrow(() -> new LocalNotFoundException(id));
+    }
+
+    public Page<LocalDTO> listPaginatedLocals(Pageable pageable, UserDetails userInSession) throws PersonNotFoundException {
+        Person userInSessionEntity = this.personRepository.findByEmailAndDeletedFalse(userInSession.getUsername())
+                .orElseThrow(() -> new PersonNotFoundException(userInSession.getUsername()));
+        Long id = userInSessionEntity.getId();
+        return this.localRepository.findByDeletedFalseAndOwnerId(pageable, id).map(LocalDTO::new);
+    }
+
     @Transactional
     public LocalDTO createLocal(CreateLocalForm form, UserDetails userInSession) throws PersonNotFoundException {
         Person owner = this.personRepository.findByEmailAndDeletedFalse(userInSession.getUsername())
                 .orElseThrow(() -> new PersonNotFoundException(userInSession.getUsername()));
         Local persistedLocal = this.localRepository.save(new Local(form, owner));
         return new LocalDTO(persistedLocal);
+    }
+
+    @Transactional
+    public void deleteLocal(Long id, UserDetails userInSession) throws LocalNotFoundException {
+        Local localForDelete = this.localRepository.findByIdAndDeletedFalseAndOwnerEmail(id, userInSession.getUsername())
+                .orElseThrow(() -> new LocalNotFoundException(id));
+
+        localForDelete.markAsDeleted();
+
+    }
+
+    @Transactional
+    public LocalDTO updateLocal(Long id, UpdateLocalForm form, UserDetails userInSession) throws LocalNotFoundException {
+        Local localForUpdate = this.localRepository.findByIdAndDeletedFalseAndOwnerEmail(id, userInSession.getUsername())
+                .orElseThrow(() -> new LocalNotFoundException(id));
+
+        localForUpdate.updateAvailableAttributes(form);
+        return new LocalDTO(localForUpdate);
+    }
+
+    public Page<LocalDTO> listPaginatedLocalsNoOwner(Pageable pageable) {
+        return this.localRepository.findByDeletedFalse(pageable).map(LocalDTO::new);
     }
 }
