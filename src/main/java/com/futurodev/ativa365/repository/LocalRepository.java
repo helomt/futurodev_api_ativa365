4c4ce21aae80ab243f3a6223838e73c0c11b3fc0
Heloisa Tavares <heloisamt@gmail.com>
2024-08-16 22:58:40 -0300

    Criação de metódos personalizados de busca por id, com validação do email do dono e deleted false

--- src/main/java/com/futurodev/ativa365/repository/LocalRepository.java
+++ src/main/java/com/futurodev/ativa365/repository/LocalRepository.java
@@ -1,7 +1,17 @@
 package com.futurodev.ativa365.repository;
 
 import com.futurodev.ativa365.model.Local;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LocalRepository extends JpaRepository<Local, Long> {
+
+    Page<Local> findByDeletedFalseAndOwnerId(Pageable pageable, Long id);
+
+    Optional<Local> findByIdAndDeletedFalseAndOwnerEmail(Long id, String email);
+
+    Page<Local> findByDeletedFalse(Pageable pageable);
 }
